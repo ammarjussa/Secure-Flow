e972502bd3d2032e4546de4d4a5715a7c8287b82
Ammar Jussa <ammar26497@gmail.com>
2023-06-22 19:48:30 +0100

    UI improvement for login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,86 +1,63 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Handle login logic here
-  };
-
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-105px)] bg-gray-100">
-      <div className="w-full max-w-md">
-        <h1 className="text-3xl text-center text-gray-800 font-semibold mb-6">
-          SecureFlow
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        >
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
+    <div className="flex items-center justify-between min-h-[calc(100vh-114px)] bg-white">
+      <div className="flex flex-col justify-center md:flex-row w-full max-w-md md:max-w-5xl">
+        <div className="md:w-1/2">
+          <div className="w-full max-w-md">
+            <h2 className="text-3xl font-bold text-center mb-6">
+              Welcome to SecureFlow
+            </h2>
+            <form className="px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <input
+                  className="shadow appearance-none bg-gray-100 border rounded-lg w-full py-2 px-3 text-gray-800  focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email address"
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="shadow appearance-none border bg-gray-100 rounded-lg w-full py-2 px-3 text-gray-800 focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Sign In
+                </button>
+                <a
+                  className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+            </form>
           </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign In
-            </button>
-            <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="/forgot-password"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy; {new Date().getFullYear()} SecureFlow. All rights reserved.
-        </p>
+        </div>
+      </div>
+      <div className="md:w-1/3 absolute right-0 bg-gray-200 p-8 flex flex-col justify-center items-end h-screen">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h3 className="text-2xl font-bold mb-4">
+            New here? Sign up and discover a great amount of new opportunities!
+          </h3>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
     </div>
   );
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-500 text-white py-3">
+    <header className="bg-white text-gray-400 py-3">
       <div className="container mx-auto px-4">
         <h1 className="text-2xl font-bold">SecureFlow</h1>
       </div>
