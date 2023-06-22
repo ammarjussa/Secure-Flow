780f2f3bf1984b25eb6246ac0355882289afafd3
Ammar Jussa <ammar26497@gmail.com>
2023-06-23 00:31:10 +0100

    Sign up page created

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -3,18 +3,18 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 
-const LoginPage: React.FC = () => {
+const SignUpPage: React.FC = () => {
   const router = useRouter();
   return (
     <div className="flex items-center justify-between min-h-[calc(100vh-110px)] bg-white">
-      <div className="flex flex-col justify-center md:flex-row w-full max-w-md md:max-w-5xl">
+      <div className="flex flex-col justify-end pl-40 ml-40 md:flex-row w-full max-w-md md:max-w-5xl">
         <div className="md:w-1/2">
-          <div className="w-full max-w-md">
+          <div className="w-full max-w-md flex flex-col">
             <h2 className="text-3xl font-bold text-center mb-6">
-              Login to your account
+              Create Free Account
             </h2>
             <div className="flex flex-col items-center">
-              <h5>Login using social networks</h5>
+              <h5>Sign up using social networks</h5>
               <div className="flex flex-row items-center">
                 <img
                   alt="google"
@@ -52,19 +52,22 @@ const LoginPage: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="md:w-1/3 absolute right-0 bg-gray-200 p-8 flex flex-col justify-center items-end h-screen">
+      <div className="md:w-1/3 absolute left-0 bg-gray-200 p-8 flex flex-col justify-center items-end h-screen">
+        <div className="container mx-auto px-4 absolute left-0 top-2 text-white">
+          <h1 className="text-2xl font-bold">SecureFlow</h1>
+        </div>
         <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="text-2xl font-bold mb-4">New here?</h1>
+          <h1 className="text-2xl font-bold mb-4">One of us?</h1>
           <p className="mb-4">
-            Sign up and discover a great amount of new opportunities!
+            If you already have an account, just sign in. We've missed you!
           </p>
           <div className="flex flex-col items-center">
             <button
               className="text-blue-500 hover:text-blue-700 bg-white self-center font-bold py-2 px-10 rounded-3xl focus:outline-none focus:shadow-outline"
               type="button"
-              onClick={() => router.push("/signup")}
+              onClick={() => router.push("/login")}
             >
-              Sign Up
+              Sign in
             </button>
           </div>
         </div>
@@ -73,4 +76,4 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+export default SignUpPage;
