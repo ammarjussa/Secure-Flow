700ddd98a291d2d3afee250d2750460f2ba0824b
Ammar Jussa <ammar26497@gmail.com>
2023-07-02 20:52:12 +0100

    firebase basic login feature added

---
+++ client/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useContext, useState, useEffect } from "react";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import firebase_app from "@/firebase/firebase";
+
+const auth = getAuth(firebase_app);
+
+export const AuthContext = createContext({});
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }: any) => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
+};
